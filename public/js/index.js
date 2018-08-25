9487095c864eeaf76d82bbb132d5a09853049dd9
Brady Shinners <brady.shinners@gmail.com>
2018-08-25 13:46:35 -0500

    page update on submit function complete. Needs filling with proper fields

--- public/js/index.js
+++ public/js/index.js
@@ -37,24 +37,91 @@ var API = {
 var refreshExamples = function() {
   API.getExamples().then(function(data) {
     var $examples = data.map(function(example) {
+
+      var $card = $("<div>")
+        .addClass("card");
+      
+      var $cardHd = $("<div>")
+        .addClass("card-header");
+
+      var $ul = $("<ul>")
+        .addClass("nav nav-tabs card-header-tabs")
+
+      for (var i =0; i< 3; i++) {
+        var $li = $("<li>")
+        .addClass("nav-item");
+
       var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
+        .addClass("nav-link active")
+        .text("VT" + i)
+        .attr("href", "#");
 
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": example.id
-        })
-        .append($a);
+        $li.append($a);
+        $ul.append($li);
+      };
 
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
+      $cardHd.append($ul);
 
-      $li.append($button);
+      var $cardBod = $("<div>")
+        .addClass("card-body");
+      
+      var $p = $("<p>")
+        .addClass("card-title phraseDisp")
+        .text("The phrases will go here.");
 
-      return $li;
+      var $row = $("<div>")
+        .addClass("row");
+
+      var $col1 = $("<div>")
+        .addClass("col-auto");
+
+      var $trans = $("<div>")
+        .addClass("card-text ml-4 transDisp")
+        .text("Leading translation will go here");
+
+      $col1.append($trans);
+      $row.append($col1);
+
+      var $col2 = $("<div>")
+        .addClass("col-auto");
+
+      var $voteButt = $("<button>")
+        .addClass("col-auto btn btn-warning voteTotal")
+        .text("VN#");
+      
+      var $downVote = $("<button>")
+        .addClass("col-auto btn btn-primary upBtn")
+        .text("^");
+
+      var $upVote = $("<button>")
+        .addClass("col-auto btn btn-danger downBtn")
+        .text("V");
+
+      $col2.append($voteButt);
+      $col2.append($downVote);
+      $col2.append($upVote);
+
+      $row.append($col2);
+
+      var $row2 = $("<div>")
+        .addClass("row");
+
+      var $phName = $("<div>")
+        .addClass("small col-3 phraseUserName")
+        .text("Submitted by: username");
+
+      var $transName = $("<div>")
+        .addClass("small col-3 offset-6 transUserName")
+        .text("Translation by: username");
+
+
+      $row2.append($phName, $transName);
+
+      $cardBod.append($p, $row, $row2);
+      
+      $card.append($cardHd, $cardBod);
+
+      return $card;
     });
 
     $exampleList.empty();
