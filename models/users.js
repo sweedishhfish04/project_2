ad7f79fd3bba8fd532e5c2a43c0b00060dd61bf5
Timothy Smith <timsmith78@yahoo.com>
2018-08-26 22:58:47 -0500

    DB model update

--- models/users.js
+++ models/users.js
@@ -2,8 +2,13 @@ module.exports = function (sequelize, DataTypes) {
     var User = sequelize.define("User", {
         username: DataTypes.STRING,
         password: DataTypes.STRING,
-        nativeLang: DataTypes.STRING,
-        knownLang: DataTypes.STRING
+        lang1: DataTypes.STRING,
+        lang1Level: DataTypes.INTEGER,
+        lang2: DataTypes.STRING,
+        lang2Level: DataTypes.INTEGER,
+        lang3: DataTypes.STRING,
+        lang3Level: DataTypes.INTEGER,
+        nativeLang: DataTypes.STRING
     });
     return User;
 };
